e919ce79a42890c535f16428d68000f789758705
sergdsv <serg.dsv@gmail.com>
2019-11-01 15:56:42 +0200

    add fonts

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="/images/favicon.png">
 
 </head>
